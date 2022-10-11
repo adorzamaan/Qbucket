6c4361190105c09e2b9aacd678c1c8a82155a929
Ador Zamaan <adorzamaan@gmail.com>
2022-10-11 11:48:29 +0600

    responsive issue fix

--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -11,7 +11,7 @@ const QuizDetails = () => {
   return (
     <div>
       <h3 className="font-bold text-2xl py-4">Quiz of {name}</h3>
-      <div className="container mx-auto justify-center grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-12">
+      <div className="container mx-auto justify-center grid lg:grid-cols-2  sm:grid-cols-1 gap-12">
         {questions.map((question) => (
           <SingleQuiz question={question} key={question.id}></SingleQuiz>
         ))}
--- src/Components/SingleQuiz/SingleQuiz.js
+++ src/Components/SingleQuiz/SingleQuiz.js
@@ -1,14 +1,24 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const SingleQuiz = ({ question }) => {
   const answerOption = question.options;
   return (
-    <div>
+    <div className="container">
       <div className="card  bg-base-100 shadow-xl p-5">
         <div className="card-body">
-          <h2 className="card-title my-4 py-2 font-medium text-purple-700">
-            {question.question}
-          </h2>
+          <div className="flex justify-between items-center">
+            <h2
+              className="card-title my-4 py-2 font-medium text-purple-700"
+              style={{ fontSize: "15px" }}
+            >
+              {question.question}
+            </h2>
+            <p>
+              <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            </p>
+          </div>
           <div className="text-center grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
             {answerOption.map((answer) => (
               <div className="flex card relative border m-3 items-center py-5 px-2 hover:border-purple-900 hover:border-2 hover:duration-75 hover:bg-gray-600 hover:text-white">
